e2f5e797af69489b4e5d1bebfd36f58286db9349
Ayush <29029116+aydwi@users.noreply.github.com>
2017-12-27 04:20:48 +0530

    Color code as file name

--- colors/process.sh
+++ colors/process.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+printf "Specified color index is %d \n\n" $2
+printf "Processing...\n\n"
+
 f1="xyc.csv"
 f2="u_xyc.csv"
 f3="reqcol.csv"
-f4="fin.csv"
+
+col_arr=("FFFFFF.csv" "E4E4E4.csv" "888888.csv" "222222.csv" \
+"FFA7D1.csv" "E50000.csv" "E59500.csv" "A06A42.csv" \
+"E5D900.csv" "94E044.csv" "02BE01.csv" "00E5F0.csv" \
+"0083C7.csv" "0000EA.csv" "E04AFF.csv" "820080.csv")
+
+f4=${col_arr[$2]}
 
 cut -d, -f1-2 --complement $1 > $f1
 sort -u $f1 > $f2
@@ -24,3 +33,5 @@ fi
 if [ -f $f3 ] ; then
     rm $f3
 fi
+
+printf "Finished writing file with name %s\n\n" $f4
