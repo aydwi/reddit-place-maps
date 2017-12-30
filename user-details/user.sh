1c7af149800b445759167ea506b6f6197fc78017
Ayush <29029116+aydwi@users.noreply.github.com>
2017-12-30 15:17:13 +0530

    csv headers

--- user-details/user.sh
+++ user-details/user.sh
@@ -3,6 +3,8 @@
 f="$2.csv"
 
 hash=`echo -n $2 | openssl dgst -binary -sha1 | openssl base64`
-grep -P "^[\d]+,$hash" $1 > $f
+
+echo "timestamp,hash,x,y,color" > $f
+grep -P "^[\d]+,$hash" $1 >> $f
 
 printf "Finished writing history of %s to file!\n\n" $2
